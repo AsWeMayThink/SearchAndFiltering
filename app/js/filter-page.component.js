8e14994ce9d1a79235e3e9c62951ab429d44f94d
John Munsch <john.munsch@gmail.com>
2019-08-31 12:45:10 -0500

    Got both genre and nationality filters working.

--- app/js/filter-page.component.js
+++ app/js/filter-page.component.js
@@ -20,6 +20,9 @@ export class FilterPage extends LitElement {
     this.allGenres = new Set();
     this.allNationalities = new Set();
 
+    this.selectedGenres = new Set();
+    this.selectedNationalities = new Set();
+
     GreatArtists.forEach(artist => {
       // Break the years apart into separate fields.
 
@@ -38,10 +41,87 @@ export class FilterPage extends LitElement {
 
     this.allGenres = Array.from(this.allGenres);
     this.allNationalities = Array.from(this.allNationalities);
+
+    this.collection = new PourOver.Collection(GreatArtists);
+
+    let genresFilter = PourOver.makeInclusionFilter('genre', this.allGenres);
+    let nationalitiesFilter = PourOver.makeInclusionFilter(
+      'nationality',
+      this.allNationalities
+    );
+
+    this.collection.addFilters([genresFilter, nationalitiesFilter]);
+
+    this.filter();
   }
 
   createRenderRoot() {
     return this;
+  }
+
+  toggleGenre(e, genre) {
+    if (e.target.checked) {
+      // Add the genre to the list of those to include.
+      this.selectedGenres.add(genre);
+    } else {
+      // Remove the genre from the list of those to include.
+      this.selectedGenres.delete(genre);
+    }
+
+    this.filter();
+  }
+
+  toggleNationality(e, nationality) {
+    if (e.target.checked) {
+      // Add the nationality to the list of those to include.
+      this.selectedNationalities.add(nationality);
+    } else {
+      // Remove the nationality from the list of those to include.
+      this.selectedNationalities.delete(nationality);
+    }
+
+    this.filter();
+  }
+
+  filter() {
+    let matches = this.collection.getAllItems();
+
+    // Go through all the genres and OR together their filter results.
+    if (this.selectedGenres.size) {
+      let genres = Array.from(this.selectedGenres).reduce((filter, genre) => {
+        if (!filter) {
+          return this.collection.filters.genre.getFn(genre);
+        } else {
+          return filter.or(this.collection.filters.genre.getFn(genre));
+        }
+      }, null);
+
+      matches = matches.and(genres);
+    }
+
+    // Then do the same for the nationalities.
+    if (this.selectedNationalities.size) {
+      let nationalities = Array.from(this.selectedNationalities).reduce(
+        (filter, nationality) => {
+          if (!filter) {
+            return this.collection.filters.nationality.getFn(nationality);
+          } else {
+            return filter.or(
+              this.collection.filters.nationality.getFn(nationality)
+            );
+          }
+        },
+        null
+      );
+
+      matches = matches.and(nationalities);
+    }
+
+    // And mark all the appropriate items.
+    GreatArtists.forEach(artist => (artist.show = false));
+    matches.cids.forEach(cid => (GreatArtists[cid].show = true));
+
+    this.requestUpdate();
   }
 
   render() {
@@ -65,7 +145,11 @@ export class FilterPage extends LitElement {
               ${this.allGenres.map(
                 genre => html`
                   <label class="radio">
-                    <input type="radio" name="answer" />
+                    <input
+                      type="checkbox"
+                      name="answer"
+                      @change="${e => this.toggleGenre(e, genre)}"
+                    />
                     ${genre}
                   </label>
                 `
@@ -78,7 +162,11 @@ export class FilterPage extends LitElement {
               ${this.allNationalities.map(
                 nationality => html`
                   <label class="radio">
-                    <input type="radio" name="answer" />
+                    <input
+                      type="checkbox"
+                      name="answer"
+                      @change="${e => this.toggleNationality(e, nationality)}"
+                    />
                     ${nationality}
                   </label>
                 `
@@ -87,14 +175,17 @@ export class FilterPage extends LitElement {
           </div>
 
           <div class="columns is-multiline is-mobile">
-            ${GreatArtists.map(
-              artist =>
-                html`
+            ${GreatArtists.map(artist => {
+              if (artist.show) {
+                return html`
                   <div class="column is-one-quarter">
                     <artist-card .artist="${artist}"></artist-card>
                   </div>
-                `
-            )}
+                `;
+              } else {
+                return html``;
+              }
+            })}
           </div>
         </div>
       </div>
